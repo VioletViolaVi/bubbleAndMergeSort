fad8f1bec1b13991d0e821955e62b2494fae3f8e
VioletViola <vivian.v18723@gmail.com>
2021-09-27 21:15:03 +0100

    Add ability for users to choose between the sorting types.

--- src/com/bubbleandmergesort/BubbleSort.java
+++ src/com/bubbleandmergesort/BubbleSort.java
@@ -59,4 +59,3 @@ public class BubbleSort {
         System.out.println();
     }
 }
-
--- src/com/bubbleandmergesort/Main.java
+++ src/com/bubbleandmergesort/Main.java
@@ -1,16 +1,29 @@
 package com.bubbleandmergesort;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /* Bubble Sort */
-        BubbleSort.generateRandArrayOfNums();
+        // allows user to input response
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which sorting method would you like? Enter Bubble or Merge?");
+        String userResponse = scanner.next();
 
-        System.out.println("\n");
-
-        /* Merge Sort */
-        MergeSort.generateRandomArrayOfInts();
+        // continues until a sorting type is selected
+        boolean continueSorting = true;
+        while (continueSorting) {
+            if (userResponse.equalsIgnoreCase("Bubble")) {
+                /* Bubble Sort */
+                BubbleSort.generateRandArrayOfNums();
+                continueSorting = false;
+            } else if (userResponse.equalsIgnoreCase("Merge")) {
+                /* Merge Sort */
+                MergeSort.generateRandomArrayOfInts();
+                continueSorting = false;
+            } else {
+                System.out.println("Please choose between Bubble or Merge.");
+                userResponse = scanner.next();
+            }
+        }
     }
 }
