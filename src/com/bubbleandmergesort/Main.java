65ab27fc9ede9b992ce8e6c8386ae456f55d1f3f
VioletViola <vivian.v18723@gmail.com>
2021-09-27 17:17:42 +0100

    Add code in main method to call bubble sort method.

--- src/com/bubbleandmergesort/Main.java
+++ src/com/bubbleandmergesort/Main.java
@@ -1,7 +1,22 @@
 package com.bubbleandmergesort;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        // empty arraylist of INTEGERS made - these are INTEGERS!!!
+        ArrayList<Integer> allNumbers = new ArrayList<>();
 
+        // random numbers
+        Random random = new Random();
+
+        for(int i = 0; i < 8; i++){
+            BubbleSort bubbleSort = new BubbleSort(random.nextInt(101));
+            allNumbers.add(bubbleSort.getAnyNum());
+        }
+
+        // calls the sorting method made to show the final (sorted) answer
+        BubbleSort.bubbleSorter(allNumbers);
     }
 }
