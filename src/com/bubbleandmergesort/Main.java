b8e43b7ee3b8f9b80c99588f4f6e9b9bb068a026
VioletViola <vivian.v18723@gmail.com>
2021-09-27 18:57:19 +0100

    Add class and methods for merge sorting.

--- src/com/bubbleandmergesort/Main.java
+++ src/com/bubbleandmergesort/Main.java
@@ -5,18 +5,30 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // empty arraylist of INTEGERS made - these are INTEGERS!!!
+        // Bubble Sort
+        // empty arraylist of integers
         ArrayList<Integer> allNumbers = new ArrayList<>();
-
         // random numbers
         Random random = new Random();
-
         for(int i = 0; i < 8; i++){
             BubbleSort bubbleSort = new BubbleSort(random.nextInt(101));
             allNumbers.add(bubbleSort.getAnyNum());
         }
-
         // calls the sorting method made to show the final (sorted) answer
         BubbleSort.bubbleSorter(allNumbers);
+
+        System.out.println("\n");
+
+        // Merge Sort
+        int array[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+        System.out.println("Before merge sorting: ");
+        MergeSort.printArray(array);
+
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(array, 0, array.length - 1);
+
+        System.out.println("\nAfter merge sorting: ");
+        MergeSort.printArray(array);
     }
 }
