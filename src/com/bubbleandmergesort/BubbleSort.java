03dfbaee13a9566b3e7dad28f94283d50514f7a9
VioletViola <vivian.v18723@gmail.com>
2021-09-27 17:15:12 +0100

    Complete code for bubble sorting.

--- src/com/bubbleandmergesort/BubbleSort.java
+++ src/com/bubbleandmergesort/BubbleSort.java
@@ -1,4 +1,48 @@
 package com.bubbleandmergesort;
 
+import java.util.ArrayList;
+
 public class BubbleSort {
+    private int anyNum;
+
+    public BubbleSort(int anyNum) {
+        this.anyNum = anyNum;
+    }
+
+    public int getAnyNum() {
+        return anyNum;
+    }
+
+    public static void bubbleSorter(ArrayList<Integer> intArray) {
+        // length of array
+        int intArrayLength = intArray.size();
+        // currently, empty int variable
+        int tempHolder;
+
+        // prints ints before sorting
+        System.out.println("Before bubble sorting: ");
+        for(int i = 0; i < intArray.size(); i++){
+            System.out.print(intArray.get(i) + ", ");
+        }
+        System.out.println();
+
+        // checking 2 ints at a time to put in ascending order
+        for (int i = 0; i < intArrayLength; i++) {
+            for (int j = 0; j < intArrayLength; j++) {
+                if (intArray.get(i) < intArray.get(j)) {
+                    tempHolder = intArray.get(i);
+                    intArray.set(i, intArray.get(j));
+                    intArray.set(j, tempHolder);
+                }
+            }
+        }
+
+        // prints ints after sorting
+        System.out.println("After bubble sorting: ");
+        for(int i = 0; i < intArray.size(); i++){
+            System.out.print(intArray.get(i) + ", ");
+        }
+        System.out.println();
+    }
 }
+
