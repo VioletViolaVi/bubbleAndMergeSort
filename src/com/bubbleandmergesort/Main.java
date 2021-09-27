19623f388fe4d1a5c84035c16bc24b62a8a9f0f6
VioletViola <vivian.v18723@gmail.com>
2021-09-27 22:29:44 +0100

    Add nano time method to code.

--- src/com/bubbleandmergesort/Main.java
+++ src/com/bubbleandmergesort/Main.java
@@ -13,13 +13,36 @@ public class Main {
         boolean continueSorting = true;
         while (continueSorting) {
             if (userResponse.equalsIgnoreCase("Bubble")) {
+
+                // records nano time from start of sorting
+                long nanoTimeBefore = System.nanoTime();
+
                 /* Bubble Sort */
                 BubbleSort.generateRandArrayOfNums();
+
+                // records nano time from start of sorting
+                long nanoTimeAfter = System.nanoTime();
+
+                // length of execution
+                long timeTakenToExecute = nanoTimeAfter - nanoTimeBefore;
+                System.out.println("Amount of nano seconds taken to execute: " + timeTakenToExecute);
+
                 continueSorting = false;
             } else if (userResponse.equalsIgnoreCase("Merge")) {
+
+                // records nano time from start of sorting
+                long nanoTimeBefore = System.nanoTime();
+
                 /* Merge Sort */
                 MergeSort.generateRandomArrayOfInts();
                 continueSorting = false;
+
+                // records nano time from start of sorting
+                long nanoTimeAfter = System.nanoTime();
+
+                // length of execution
+                long timeTakenToExecute = nanoTimeAfter - nanoTimeBefore;
+                System.out.println("Amount of nano seconds taken to execute: " + timeTakenToExecute);
             } else {
                 System.out.println("Please choose between Bubble or Merge.");
                 userResponse = scanner.next();
